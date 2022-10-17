1f0f65af73bcc737745a1825d83d15b5570d3e0b
setiyadi-ben <106580966+setiyadi-ben@users.noreply.github.com>
2022-10-17 13:45:16 +0700

    group sensor ke main.py. untuk dht11 masih error

---
+++ main.py
@@ -0,0 +1,36 @@
+from ast import While
+from subprocess import call
+import threading # Ref: https://www.geeksforgeeks.org/multithreading-python-set-1/
+import time
+
+class groupingSensors:
+
+    def run_potensio_sensor():
+        call(["python", "potensio.py"])
+
+    # def run_dht_sensor2():
+    #     call(["python", "sensorB.py"])
+
+    def run_hcsr04_sensor3():
+        call(["python", "sensor-hcsr04.py"])
+
+if __name__ == "__main__":
+
+    # Listing threads
+    sensor1 = threading.Thread(target = groupingSensors.run_potensio_sensor, name = "sensor1")
+    # sensor2 = threading.Thread(target = groupingSensors.run_potensio_sensor, name = "sensor2")
+    sensor3 = threading.Thread(target = groupingSensors.run_hcsr04_sensor3, name = "sensor3")
+
+    # Starting threads
+    sensor1.start()
+    # sensor2.start()
+    sensor3.start()
+
+    # Joining threads
+    sensor1.join()
+    # sensor2.join()
+    sensor3.join()
+
+    # Looping
+    time.sleep(1)
+
